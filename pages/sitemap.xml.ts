3f87836d6eab81ade71aae6e034eb44f4a458957
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2022-08-28 16:18:38 +0200

    add .env.example

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -8,7 +8,6 @@ function generateSiteMap(baseUrl: string, data: GetCategoriesProductsSlugQuery) 
 	const { categories } = data;
 	return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://jsonplaceholder.typicode.com</loc>
      </url>
